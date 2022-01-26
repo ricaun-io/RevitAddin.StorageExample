699eb37da619ff06c841352cb872b4ef95e454c5
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-01-26 13:54:29 -0300

    Add StorageProjectInfoFactory Add StorageFactory

--- RevitAddin.StorageExample/Services/StorageWallService.cs
+++ RevitAddin.StorageExample/Services/StorageWallService.cs
@@ -1,13 +1,13 @@
-﻿using Autodesk.Revit.UI;
+﻿using Autodesk.Revit.DB;
 using System;
 
 namespace RevitAddin.StorageExample.Services
 {
-    public class StorageTextService : StorageFactory<string>
+    public class StorageWallService : StorageFactory<string, Wall>
     {
         public override Guid Guid => new Guid("1E832DE4-C3F8-4DAE-84AF-450BAF593AC4");
         public override string FieldName => "Text";
-        public override string SchemaName => "StorageTextService";
+        public override string SchemaName => "StorageWallService";
         public override string VendorId => "ricaun";
     }
 }
